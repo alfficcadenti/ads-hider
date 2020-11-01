193a2e680761555a211ad536fba286b037b51aa5
alfficcadenti <alfredoficcadenti@gmail.com>
2020-11-01 06:54:37 +0000

    initial commit hide blocks in qwant

---
+++ ads-hider.js
@@ -0,0 +1,5 @@
+document.body.style.border = "5px solid red";
+const block = document.getElementsByClassName("web_add_block");
+alert(block, block[0])
+console.log(block)
+block[0].style.display = "none";
